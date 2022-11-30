199e1725e8b5cc245f6e55c9247b2aa6a9dabfc8
Matt Hadfield <30897252+MattHadfield113@users.noreply.github.com>
2022-11-30 10:42:53 +0000

    Update fwkaliases.sh

--- fwkaliases.sh
+++ fwkaliases.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-FILE=~/fwkaliases.sh
-if [ -f "$FILE" ]; then
-    echo "$FILE exists. Skipping"
-else 
-    echo "$FILE does not exist. Downloading and setting as runnable"
-    wget https://raw.githubusercontent.com/FrameworkDesign/linuxaliases/main/fwkaliases.sh -O ~/fwkaliases.sh
-    chmod +x ~/fwkaliases.sh
-fi
+echo "Downloading Latest Runner for Aliases"
+wget https://raw.githubusercontent.com/FrameworkDesign/linuxaliases/main/fwkaliases.sh -O ~/fwkaliases.sh
+chmod +x ~/fwkaliases.sh
 
+echo "Downloading and installing Aliases"
 wget https://raw.githubusercontent.com/FrameworkDesign/linuxaliases/main/.bash_aliases -O ~/.bash_aliases
 . ~/.bashrc
