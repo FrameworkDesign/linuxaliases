1640e195fad540bebffb00631100061483bf3aa2
Matt Hadfield <30897252+MattHadfield113@users.noreply.github.com>
2022-12-08 10:06:59 +0000

    Update fwkaliases.sh

--- fwkaliases.sh
+++ fwkaliases.sh
@@ -6,4 +6,4 @@ chmod +x ~/fwkaliases.sh
 
 echo "Downloading and installing Aliases"
 wget https://raw.githubusercontent.com/FrameworkDesign/linuxaliases/main/.bash_aliases -O ~/.bash_aliases
-. ~/.bashrc
+source ~/.bashrc
